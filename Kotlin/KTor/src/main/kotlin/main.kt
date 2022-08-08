2f8addd664b7691b2cb7abb12b5560ea9a095b59
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-08-08 00:36:27 +0000

    new

--- Kotlin/KTor/src/main/kotlin/main.kt
+++ Kotlin/KTor/src/main/kotlin/main.kt
@@ -1,5 +1,41 @@
-
+import java.util.*
 
 fun main() {
-    println("Hello world")
+    val scanner = Scanner(System.`in`)
+    val number: Int = scanner.nextInt()
+//
+   var sum: Int = 0
+//    val average: Int = 0
+//
+//    for (num in number.rangeTo(1000)){
+//        sum +=number
+//    }
+
+
+//    val numArray = doubleArrayOf(45.3, 67.5, -45.6, 20.34, 33.0, 45.6)
+//    var sum = 0.0
+//
+//    for (num in numArray) {
+//        sum += num
+//    }
+//
+//    val average = sum / numArray.size
+//    println("The average is: %.2f".format(average))
+
+
+    var flag = false
+    for (i in 2..number / 2) {
+        // condition for nonprime number
+        if (number % i == 0) {
+            flag = true
+            sum += number
+            break
+        }
+    }
+
+    if (!flag)
+        println("$number is a prime number.")
+    else
+        println("$number is not a prime number.")
+
 }
