57e6e0b19f7a7195e70f25947f4909fff543a433
Kelvin Eduful <keduful@st.ug.edu.gh>
2023-06-01 10:20:35 +0000

    finally the stack worked

--- Java/Stack/src/main/java/com/eduful9877/Main.java
+++ Java/Stack/src/main/java/com/eduful9877/Main.java
@@ -7,6 +7,25 @@ package com.eduful9877;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Stack<Integer> test = new Stack<> ();
+        test.push ( 2 );
+        test.push ( 3 );
+        test.push ( 4 );
+        test.push ( 5 );
+        test.push ( 6 );
+        test.push ( 7 );
+        test.push ( 8 );
+
+        for (int i = 0; i < test.list.size ( ); i++) {
+            System.out.println ( test.list.get(i) );
+        }
+
+        test.pop ();
+        test.pop ();
+        System.out.println ("Remove some" );
+
+        for (int i = 0; i < test.list.size ( ); i++) {
+            System.out.println ( test.list.get(i) );
+        }
     }
 }
--- Java/Stack/src/main/java/com/eduful9877/Stack.java
+++ Java/Stack/src/main/java/com/eduful9877/Stack.java
@@ -1,4 +1,21 @@
 package com.eduful9877;
 
+import java.util.ArrayList;
+
 // push and pop, isEmpty using LinkedList
-public class Stack {}
+public class Stack<T> {
+
+    public ArrayList<T> list;
+
+    public Stack() {
+        list = new ArrayList<>();
+    }
+
+    public void push(T element) {
+        list.add(list.size(), element);
+    }
+
+    public void pop() {
+        list.remove(list.size () - 1 );
+    }
+}
