6b9be5cc82f5cecacb131de67cf4513c5d18bd96
Kelvin Eduful <keduful@st.ug.edu.gh>
2023-06-02 22:34:50 +0000

    bugs which can give headache

---
+++ Java/Linked-Structures/src/main/java/com/eduful9877/queue/Main.java
@@ -0,0 +1,14 @@
+package com.eduful9877.queue;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String sit = "HackerRank ";
+        String seen;
+        Scanner scanner = new Scanner ( System.in );
+        seen = scanner.nextLine ();
+
+        System.out.println (sit + seen );
+    }
+}
--- Java/Linked-Structures/src/main/java/com/eduful9877/queue/QueueImpl.java
+++ Java/Linked-Structures/src/main/java/com/eduful9877/queue/QueueImpl.java
@@ -1,7 +1,36 @@
 package com.eduful9877.queue;
 
+import java.util.Scanner;
+
 public class QueueImpl {
     public static void main(String[] args) {
-        System.out.println("Hello Queue");
+        int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";
+        Scanner scan = new Scanner ( System.in );
+
+        /* Declare second integer, double, and String variables. */
+        int inputInteger;
+        double inputDouble;
+        String inputString;
+
+        /* Read and save an integer, double, and String to your variables.*/
+        // Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
+        inputInteger = scan.nextInt();
+        inputDouble = scan.nextDouble();
+        inputString = scan.nextLine();
+
+        /* Print the sum of both integer variables on a new line. */
+        System.out.println(i + inputInteger);
+
+        /* Print the sum of the double variables on a new line. */
+        System.out.printf("%,.1f\n",(d + inputDouble));
+
+        /** Concatenate and print the String variables on a new line;
+         the 's' variable above should be printed first. */
+        String concat = s + inputString;
+        System.out.println ( concat );
+
+//        scan.close();
     }
 }
