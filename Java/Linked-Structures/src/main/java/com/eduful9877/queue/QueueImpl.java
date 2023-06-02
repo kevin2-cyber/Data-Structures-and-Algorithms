f3ceb40363462087c233a86598a9c730739b4cf0
Kelvin Eduful <keduful@st.ug.edu.gh>
2023-06-02 13:53:59 +0000

    creating a queue

---
+++ Java/Linked-Structures/src/main/java/com/eduful9877/queue/QueueImpl.java
@@ -0,0 +1,7 @@
+package com.eduful9877.queue;
+
+public class QueueImpl {
+    public static void main(String[] args) {
+        System.out.println("Hello Queue");
+    }
+}
---
+++ Java/Linked-Structures/src/main/java/com/eduful9877/stack/StackImpl.java
@@ -0,0 +1,31 @@
+package com.eduful9877.stack;
+
+/**
+ * Author = Kelvin Eduful
+ * StudentId = 10889877
+ */
+
+public class StackImpl {
+    public static void main(String[] args) {
+        Stack<Integer> test = new Stack<> ();
+        test.push ( 2 );
+        test.push ( 3 );
+        test.push ( 4 );
+        test.push ( 5 );
+        test.push ( 6 );
+        test.push ( 7 );
+        test.push ( 8 );
+
+        for (int i = 0; i < test.list.size ( ); i++) {
+            System.out.println ( test.list.get(i) );
+        }
+
+        test.pop ();
+        test.pop ();
+        System.out.println ("Remove some" );
+
+        for (int i = 0; i < test.list.size ( ); i++) {
+            System.out.println ( test.list.get(i) );
+        }
+    }
+}
