98111b6e1cfffdc9b45f221d0577c086576abafa
Kelvin Eduful <keduful@st.ug.edu.gh>
2023-08-01 21:56:07 +0000

    Breadth First Search

---
+++ Java/SNS/src/BFS.java
@@ -0,0 +1,49 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BFS {
+    public static class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static void bfs(TreeNode root) {
+        if (root == null)
+            return;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            System.out.println(node.val + " ");
+
+            if (node.left != null)
+                queue.offer(node.left);
+
+            if (node.right != null)
+                queue.offer(node.right);
+        }
+    }
+
+    public static void main(String[] args) {
+        // create a simple binary tree
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(7);
+
+        // Perform BFS traversal
+        System.out.println("BFS traversal: ");
+        bfs(root);
+        System.out.println();
+    }
+}
