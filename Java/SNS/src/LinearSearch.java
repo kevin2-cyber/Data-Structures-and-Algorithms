82fc16491a8ac869b04a662dc8b7739adb873e7b
Kelvin Eduful <keduful@st.ug.edu.gh>
2023-08-01 19:09:08 +0000

    tried linear search using Scanner class

--- Java/SNS/src/LinearSearch.java
+++ Java/SNS/src/LinearSearch.java
@@ -1,12 +1,15 @@
+import java.util.Scanner;
+
 public class LinearSearch {
     public static void main(String[] args) {
-        int[] numbers = {4,7,1,9,8,3,5,0};
+        int[] numbers = {0,1,2,3,4,5,6,6,7,8,9};
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
 
         // count through numbers
-        for (int num :
-                numbers) {
-            if (num == 8) {
-                System.out.println("-> " + num + " <-");
+        for (int num : numbers) {
+            if (num == number) {
+                System.out.print("-> " + num + " <-");
                 break;
             }
             System.out.print(num + ", ");
