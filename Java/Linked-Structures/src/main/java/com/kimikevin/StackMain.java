85f456381011b9151b5893982d7ce1f61ce487ba
Kelvin Eduful <keduful@st.ug.edu.gh>
2023-08-03 23:43:00 +0000

    coding example for stack

---
+++ Java/Linked-Structures/src/main/java/com/kimikevin/StackMain.java
@@ -0,0 +1,19 @@
+package com.kimikevin;
+
+import java.util.Stack;
+
+public class StackMain {
+    public static void main(String[] args) {
+        Stack<String> stack = new Stack<>();
+
+        stack.push("BMW");
+        stack.push("Corvette");
+        stack.push("Lambo");
+
+        System.out.println("peek: " + stack.peek());
+        System.out.println("pop: " + stack.pop());
+
+        stack.push("Toyota Camri");
+        System.out.println("peek: " + stack.peek());
+    }
+}
